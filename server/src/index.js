7c7f9a31d406c088c54424f019af0c76b6e2b9cf
Ganesh Bagra <ganeshbagra003@gmail.com>
2025-10-25 15:35:20 +0530

    Db connection successfully

---
+++ server/src/index.js
@@ -0,0 +1,25 @@
+
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+import {app} from './app.js';
+
+dotenv.config({
+    path : './.env',
+    override : true
+})
+
+const port = process.env.PORT || 4000;
+
+connectDB()
+.then(() => {
+    app.get("/", (req,res) => {
+        res.send("App Running")
+    })
+
+    app.listen( port,() => {
+        console.log("Server running on port : ", port)
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed : ", err)
+});
