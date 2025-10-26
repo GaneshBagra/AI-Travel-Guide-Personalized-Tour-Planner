f0c2bdd80df9d079fb0732407c27c005e21c03de
Ganesh Bagra <ganeshbagra003@gmail.com>
2025-10-27 00:59:47 +0530

    cors error fixed

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -104,6 +104,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
+    sameSite: "none",
   };
 
   return res
@@ -142,6 +143,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
+    sameSite: "none",
   };
 
   return res
