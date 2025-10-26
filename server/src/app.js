fa04f7e14af0c9ba4057c663fe0c7fdb37bd0889
Ganesh Bagra <ganeshbagra003@gmail.com>
2025-10-27 00:45:22 +0530

    cors error fixed

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,7 @@ const app = express()
 
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://ai-travel-app-bmgug.ondigitalocean.app"], 
+    origin: ["http://localhost:5173", "https://app-frontend-qdken.ondigitalocean.app/"], 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
